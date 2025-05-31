6d4d548308a5dfe23efee2b3e8eea38433d2a42b
sandeep <sandeepsai530@gmail.com>
2025-05-31 12:04:00 +0000

    added files

--- challenge-2.sh
+++
@@ -1,5 +0,0 @@
-#Create a directory named devops_challenge_day_1, navigate into it, and create an empty file named day1.txt.
-
-mkdir devops_challenge_day_1
-cd devops_challenge_day_1
-touch day1.txt
--- challenge-4.sh
+++
@@ -1,7 +0,0 @@
-#Create a new user called devops_user and add them to the sudo group.
-
-sudo adduser devops_user
-sudo usermod -aG sudo devops_user
-
-#optional to verify
-getent group sudo
